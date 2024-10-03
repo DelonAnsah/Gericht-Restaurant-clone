8820caef814847727c83b6d5d22a26f268116062
d3lon-dev <delonansah87@gmail.com>
2024-10-03 14:09:28 +0000

    initial commit for Gericht Restaurant

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Navbar.css'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { MdOutlineRestaurantMenu } from 'react-icons/md'
+
+
+const Navbar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/container/Chef/Chef.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Chef.css'
+
+const Chef = () => (
+  <div>
+    Chef
+  </div>
+)
+
+export default Chef

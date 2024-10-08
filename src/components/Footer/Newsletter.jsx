d2e54697934102fa7f1463fff8123e0a50fad3ea
d3lon-dev <delonansah87@gmail.com>
2024-10-08 21:31:09 +0000

    second part of the footer added including profile links to the social media icons

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -36,7 +36,7 @@ const Newsletter = () => {
     <div className='app__newsletter'>
       <div className="app__newsletter-heading">
         <SubHeading title="Newsletter" />
-        <h1 className="headtext__cormorant">Subscribe to Our Newsletter</h1>
+        <h1 className="headtext__cormorant">Subscribe to My Newsletter</h1>
         <p className="p__opensans">And never miss latest Updates!</p>
       </div>
       <div className="app__newsletter-input flex__center">
